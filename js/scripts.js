f5a00e25e1dbbedfaf924510feba0221ee7da65b
Summer Brochtrup & Perry Eising <student@epicodus.com>
2015-09-23 14:21:57 -0700

    Add AWESOME isWinner function

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,39 @@
+var squareRoot;
+
 function Player(name, mark, spacesTaken) {
   this.name = name;
   this.mark = mark;
   this.spacesTaken = spacesTaken;
 }
 
-Player.prototype.winner = function(){
-  //create this later
-}
-
 Player.prototype.addSpace = function(space) {
   this.spacesTaken.push([space[0], space[1]]);//spacesTaken refuses to be defined
 }
+
+Player.prototype.isWinner = function(Board) {
+  var xs = []; //create array of x's
+  var ys = [];
+    for (var i = 0; i < this.spacesTaken.length; i++) {
+      xs.push(this.spacesTaken[i][0]) //0 is x value
+      ys.push(this.spacesTaken[i][1]) //1 is y value
+    }
+
+  var xString = xs.toString();
+  var yString = ys.toString();
+
+  var rowLength = Math.floor(Math.sqrt(Board.length)); //how many need to be in a row to win
+  if (this.spacesTaken.length == rowLength) {
+    if (xs.reduce(function(a, b){return (a === b)?a:(!b);}) === xs[0] || (ys.reduce(function(a, b){return (a === b)?a:(!b);}) === ys[0])) {
+      return true;
+    } else if (xString == yString){
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    return false;
+  }
+};
 
 function Space(coordinateX, coordinateY) {
   this.coordinateX = coordinateX;
@@ -22,6 +45,7 @@ Space.prototype.setMark = function (player){
 }
 
 function Board(space, number) {
+  this.number = number;
   var squareRoot = Math.floor(Math.sqrt(number));
   var newBoard = [];
   for(var i = 1; i <= squareRoot; i++) {
@@ -32,22 +56,6 @@ function Board(space, number) {
   }
   return newBoard;
 }
-
-Board.prototype.isWinner = function(player) {
-    for(var i; i < player.spacesTaken.length; i++) {
-
-    var xs = []; //create array of x's
-    var ys = [];
-    for (var i = 0; i < player.spacesTaken.length; i++)
-    {
-      xs.push(player.spacesTaken[i][0]) //0 is x value
-      ys.push(player.spacesTaken[i][1]) //1 is y value
-      }
-  }
-  //then compare
-}
-
-
 
 function Game(board, player1, player2) {
   this.board = board;
